65bcb6da40a8273f0eb3c1e06a4f436a8fabaabf
Guy McDonald <guy@goodfishgroup.com>
2024-03-05 17:50:36 +0000

    Map now displaying, but no markers yet.

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,6 +1,13 @@
 class CrimesController < ApplicationController
   def index
     @crimes = Crime.all
+
+    @markers = @crimes.geocoded.map do |crime|
+      {
+        lat: crime.latitude,
+        lng: crime.longitude,
+      }
+    end
   end
 
   def show
