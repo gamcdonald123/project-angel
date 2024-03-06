7dfd757c811f72e5b9e665a0158121120541f51f
Antonio Propato <tonyprp@gmail.com>
2024-03-06 11:59:34 +0000

    Safe places seeded

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_150355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "crimes", force: :cascade do |t|
-    t.string "category"
-    t.float "latitude"
-    t.float "longitude"
-    t.string "street"
-    t.string "month"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,6 +40,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_150355) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "crimes", force: :cascade do |t|
+    t.string "category"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "street"
+    t.string "month"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reports", force: :cascade do |t|
