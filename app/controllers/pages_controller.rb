85d04fbd8d62b09baab9123db6241f302d1e4fa3
Guy McDonald <guy@goodfishgroup.com>
2024-03-08 14:50:04 +0000

    Crimes seeded for presentation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -32,4 +32,31 @@ class PagesController < ApplicationController
     end
     @markers = crime_markers + safe_place_markers
   end
+
+  def app
+    @crimes = Crime.all
+
+    crime_markers = @crimes.map do |crime|
+      {
+        type: "crime",
+        lat: crime.latitude,
+        lng: crime.longitude,
+        info_window_html: render_to_string(partial: "crime_info_window", locals: {crime: crime}),
+        marker_html: render_to_string(partial: "crime_marker")
+      }
+    end
+
+    @safe_places = SafePlace.all
+
+    safe_place_markers = @safe_places.map do |safe_place|
+      {
+        type: "safe_place",
+        lat: safe_place.latitude,
+        lng: safe_place.longitude,
+        info_window_html: render_to_string(partial: "safe_place_info_window", locals: {safe_place: safe_place}),
+        marker_html: render_to_string(partial: "safe_place_marker")
+      }
+    end
+    @markers = crime_markers + safe_place_markers
+  end
 end
