1aaf263fc46b07f15ab10910018fca26cd913105
Mirela Mustic <mirela.mustic00@gmail.com>
2024-03-11 16:31:15 +0000

    added date and time to form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_08_112324) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_154644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_08_112324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "did_it_happen_to_you"
+    t.datetime "date_and_time"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
