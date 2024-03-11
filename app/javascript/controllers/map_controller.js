db32e45c73012fbcb9bb071f409469661321b6a9
Guy McDonald <guy@goodfishgroup.com>
2024-03-11 11:53:14 +0000

    Halfway through the tutorial for mapbox

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -27,6 +27,10 @@ export default class extends Controller {
     this.#addMarkersToMap()
     // this.#fitMapToMarkers()
     // console.log(this.geoLocate())
+    let start = "-0.07707799702848642,51.53281557674937"
+    let end = "-0.1530621981637358,51.52629852471669"
+    this.getRoute(start)
+
 
   }
 
@@ -60,7 +64,7 @@ export default class extends Controller {
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 
-  geoLocate() {
+  geoLocate(start, end) {
 
     navigator.geolocation.getCurrentPosition((data) => {
 
@@ -78,20 +82,85 @@ export default class extends Controller {
     });
   }
 
-  getRoute(end) {
+  getRoute() {
 
     const accessToken = "pk.eyJ1IjoiZ2FtY2RvbmFsZDEyMyIsImEiOiJjbHNsc25ybWkwMmJxMm1xb3U2cXhnMGhjIn0.2xgoKDVEBTSGsghazmqAeA"
 
-    let start = "-0.07707799702848642,51.53281557674937"
-    // let end = "-0.1530621981637358,51.52629852471669"
-
     const base_url = "https://api.mapbox.com/directions/v5/mapbox/walking/"
-
-
-  }
-
-  directionsTest() {
-    let url = "https://api.mapbox.com/directions/v5/driving/{coordinates}"
+    const url = `${base_url}-0.07707799702848642,51.53281557674937;-0.1530621981637358,51.52629852471669?steps=true&geometries=geojson&access_token=${accessToken}`
+    const geojson = {}
     fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        let route = data.geometry.coordinates
+        geojson = {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: route
+          }
+        }
+      })
+
+      // if the route already exists on the map, we'll reset it using setData
+
+      if (map.getSource('route')) {
+        map.getSource('route').setData(geojson);
+      }
+      // otherwise, we'll make a new request
+      else {
+        map.addLayer({
+          id: 'route',
+          type: 'line',
+          source: {
+            type: 'geojson',
+            data: geojson
+          },
+          layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+          },
+          paint: {
+            'line-color': '#3887be',
+            'line-width': 5,
+            'line-opacity': 0.75
+          }
+        });
+
+    }
+
+    map.on('load', () => {
+      // make an initial directions request that
+      // starts and ends at the same location
+      getRoute(start);
+
+      // Add starting point to the map
+      map.addLayer({
+        id: 'point',
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: [
+              {
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                  type: 'Point',
+                  coordinates: start
+                }
+              }
+            ]
+          }
+        },
+        paint: {
+          'circle-radius': 10,
+          'circle-color': '#3887be'
+        }
+      });
+      // this is where the code from the next step will go
+    });
   }
 }
