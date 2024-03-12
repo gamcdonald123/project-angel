a2b08561a95aa2fbe19d24e44b072163cef89774
Mirela Mustic <mirela.mustic00@gmail.com>
2024-03-12 17:40:47 +0000

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
   has_many :communities, through: :posts
   has_many :messages
   has_one_attached :photo
-<<<<<<< HEAD
   has_one :profile
+  has_many :comments
+  has_many :comments, through: :posts
 
   after_create :create_user_profile
 
@@ -18,8 +19,4 @@ private
   def create_user_profile
     create_profile
   end
-=======
-  has_many :comments
-  has_many :comments, through: :posts
->>>>>>> 1b80e6f3545c56a968ca19846fe08b3439f6ce7a
 end
