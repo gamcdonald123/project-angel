dca2055ff37bb79c045a5db45539059f978670ba
Nicole Farinello <nicole_mattos@live.com>
2024-03-12 11:27:23 +0000

    added the private class to the chatroom table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_03_11_154644) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_112529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,14 +42,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_154644) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-
-  create_table "communities", force: :cascade do |t|
+  create_table "chatrooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "private", default: false
   end
 
-  create_table "chatrooms", force: :cascade do |t|
+  create_table "communities", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -67,6 +65,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_154644) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -77,16 +84,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_154644) do
     t.datetime "updated_at", null: false
     t.index ["community_id"], name: "index_posts_on_community_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.string "content"
-    t.bigint "chatroom_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "reports", force: :cascade do |t|
@@ -131,9 +128,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_154644) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "posts", "communities"
-  add_foreign_key "posts", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "posts", "communities"
+  add_foreign_key "posts", "users"
   add_foreign_key "reports", "users"
 end
