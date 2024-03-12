860ece414f57273450c81492db48f3fa09cc4f4c
Antonio Propato <tonyprp@gmail.com>
2024-03-12 16:59:44 +0000

    Stimulus sends the user to the bottom page when they click on the message icon in the post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_111356) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_155644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_111356) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
