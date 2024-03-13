b62a6e2c16b5fe52ca68cea9ec6fc6ef9ab7d760
Guy McDonald <guy@goodfishgroup.com>
2024-03-13 11:14:43 +0000

    Changed marker for hospital

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -302,7 +302,7 @@ export default class extends Controller {
               "longitude": data.features[0].geometry.coordinates[0],
               "latitude": data.features[0].geometry.coordinates[1],
               "info_window_html": `<div><h5>🏥 Hospital</h5><p>🪧 ${data.features[0].properties.name}</p></div>`,
-              "marker_html": `<img height="48" width="48" alt="Logo" src="/assets/safe_place_marker.png" />`
+              "marker_html": `<img height="48" width="48" alt="Logo" src="/assets/hospital.png" />`
             };
 
           this.#addHospitalsToMap(coordinates);
